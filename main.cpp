c1a326d22bb1812fe1266ac5172e5fa34f772219
Bahar Sharafi <bsharafi@smpp.local>
2016-08-21 15:54:00 -0500

    Finished creating and implementing Multicolor_Box class to simulate both bright and dark balls. Updated Readme file to reflect this. Also fixed some bugs and did a better job with commenting. Added some lines of code usefulfor debugging and for seeing what is happening while simulation is occurring.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <random>
+#include <chrono>
 // This program defines a class which is used to simulate the probability distribution of finding fluorescently labeled DNA molecules
 // occupying multivalent protein binding sites in TIRF images. The Multicolor_box class allows for a mixture of fluorescently labeled DNA and
 // non-fluorescent biotin, both of which can bind to multivalent streptavidin.
@@ -15,16 +17,11 @@ protected:    // Each box is characterized by the number of balls it can hold, a
     int _box_size, _num_balls;
 public:
     Box(); //Default constructor, by default box is empty when created. Also by default the size is 3
-
     Box(int box_size, int num_balls); //Constructor for setting by hand the box size and number of balls
     bool isfull();   //Determine whether a box is full to capacity
-
     void add_ball();  //Add a ball to a box
-
     int size_of_box();
-
     int how_many();  //Tells you how many balls are in a box
-
 };
 
 class Multicolor_Box : public Box{
@@ -40,31 +37,40 @@ public:
     int how_many_dark();
 };
 
-int rand_0toN_minus_1(int n); //prototype declaration for choosing a random number from 0 to num_boxes - 1
+struct Ball_collection {
+protected:
+    int _num_bright_balls;
+    int _num_dark_balls;
+public:
+    string remove_ball(); //randomly removes a ball from the collection. Return whether removed ball is "bright" or "dark"
+    void populate(int num_bright_balls, int num_dark_balls);
+    bool is_collection_empty();
+};
+
+int rand_0toN_minus_1(int n); //prototype declaration for choosing a random number from 0 to n - 1
 
 int main(){
 
-
     //now we do the real work
+    srand(time(NULL)); //Set the seed for random numbers
+
     int num_boxes;
     int num_balls;
-    cout << "how many boxes would you like? ";
+    cout << "Each box can hold three balls. How many boxes would you like? ";
     cin >> num_boxes;
     cout << endl;
-    cout << "how many balls would you like? must be less than or equal to " << 3*num_boxes << " :";
-    cin >> num_balls;
+    cout << "How many balls would you like? must be less than or equal to " << 3*num_boxes << " :";
+    cin >> num_balls; // This is the number of balls you are going to distribute among the boxes
     cout << endl;
     if (num_balls > 3*num_boxes){
         cout << "You can't do that! There aren\'t enough slots in the boxes for that many balls." << endl;
         exit(-1);
     }
-    //int num_boxes = 100000; //Number of boxes (represents streptavidins)
-    //int num_balls = 10000; //Number of balls (represents the DNA) This should be less than or equal to 3 times the number of boxes
-    //Box box_collection[num_boxes]; //Create 1000 empty boxes
+
     Box *box_collection = new Box[num_boxes];
-    srand(time(NULL)); //Set the seed for random numbers
+
     int r; //box index which gets randomly chosen
-    int occupation_histogram[4] = {0,0,0,0}; //vector containing the occupation numbers of 0,1,2, and 3 balls.
+    int occupation_histogram[box_collection[0].size_of_box() + 1] = {0,0,0,0}; //vector containing the occupation numbers of 0,1,2, and 3 balls.
 
 
 
@@ -92,20 +98,42 @@ int main(){
     //Now output histogram to a file so it can be plotted by Origin.
     ofstream histogramfile;
     histogramfile.open("histogram_file.txt");
-    for (int i=0; i <= box_collection[1].size_of_box(); i++){
+    histogramfile << "Distributing " << num_balls << " balls among " << num_boxes << " boxes results in the following occupation " << endl;
+    histogramfile << "numbers of 0, 1, 2, and 3 balls." << endl;
+    for (int i=0; i <= box_collection[0].size_of_box(); i++){
         histogramfile << i << ' ' << occupation_histogram[i] << endl;
+    }
+    histogramfile << endl;
+
+    //Normalize populations of each ball count so it can be a probability
+    histogramfile << "And in terms of percentages: " << endl;
+    for (int i = 0; i <= box_collection[0].size_of_box(); i++){
+        histogramfile << i << ' ' << static_cast<double>(occupation_histogram[i])/num_boxes << endl;
+    }
+    histogramfile << endl;
+    histogramfile << "Occupation percentage of boxes that have at least one ball: " << endl;
+    int nbalob = 0; // Number of boxes with at least one ball.
+    for (int i = 1; i <= box_collection[0].size_of_box(); i++){
+        nbalob += occupation_histogram[i];
+    }
+    for (int i = 1; i <= box_collection[0].size_of_box(); i++){
+        histogramfile << i << ' ' << static_cast<double>(occupation_histogram[i])/nbalob << endl;
     }
 
     histogramfile.close();
-    delete [] box_collection;
+    delete [] box_collection; //memory cleanup
 
     Multicolor_Box tstbox; // create a Multicolor_Box object to test out class with.
+    cout << endl;
+    cout << "Just testing out the Multicolor_Box class here..." << endl;
     cout << "tstbox has " << tstbox.size_of_box() << " slots in it." << endl;
     cout << "tstbox has " << tstbox.how_many() << " total balls in it." << endl;
     tstbox.add_bright_ball();
+    cout << "Added a bright ball. How many dark balls? (should be zero) " << tstbox.how_many_dark() << endl;
     tstbox.add_dark_ball();
     cout << "tstbox has " << tstbox.how_many() << " total balls in it." << endl;
     cout << "tstbox has " << tstbox.how_many_bright() << " bright balls in it." <<endl;
+    cout << "OK, testing over." << endl << endl;
 
 
     //Now create a collection of Multicolor_Box objects
@@ -128,11 +156,98 @@ int main(){
 
     Multicolor_Box *box_collection2 = new Multicolor_Box[num_multi_boxes]; //Collection of boxes that can hold dark or light balls.
 
+    Ball_collection bc; // initialize a ball collection
+    bc.populate(num_bright_balls,num_dark_balls); //populate ball collection with the bright and dark balls
+
+    string ball_type_removed;
     //OK, now randomly populate the boxes with dark and light balls until no balls are left
-    //...needs completion
+    while (!(bc.is_collection_empty())){ //while the ball collection still has balls in it
+        ball_type_removed.assign(bc.remove_ball()); //remove a ball and store what kind of ball it is in ball_type_removed
+        if (ball_type_removed == "bright"){
+            //add a bright ball to the Box Collection
+            r = rand_0toN_minus_1(num_multi_boxes);//randomly choose a box
+            if (not(box_collection2[r].isfull())) { //if the box you chose isn't full, add a ball to it
+                box_collection2[r].add_bright_ball();
+            } else { //if the first box you chose was full, keep selecting boxes until you find one that is empty and add a ball to it
+                while (box_collection2[r].isfull()) {
+                    r = rand_0toN_minus_1(num_multi_boxes);
+                }
+                box_collection2[r].add_bright_ball();
+            }
+        } else if(ball_type_removed == "dark"){
+            //add a dark ball to box collection
+            r = rand_0toN_minus_1(num_multi_boxes);//randomly choose a box
+            if (not(box_collection2[r].isfull())) { //if the box you chose isn't full, add a ball to it
+                box_collection2[r].add_dark_ball();
+            } else { //if the first box you chose was full, keep selecting boxes until you find one that is empty and add a ball to it
+                while (box_collection2[r].isfull()) {
+                    r = rand_0toN_minus_1(num_multi_boxes);
+                }
+                box_collection2[r].add_dark_ball();
+            }
+        }
 
+    }
 
-    delete [] box_collection2;
+    //populate histogram of number of boxes with 0,1,2, or 3 bright balls in them
+    vector<int> histogram_bright(box_collection2[0].size_of_box()+1);
+
+    for (int i=0; i <= box_collection2[0].size_of_box(); i++){ //zero out bright histogram
+        histogram_bright.push_back(0);
+    }
+
+    for (int i=0; i < num_multi_boxes; i++){ //Run through the boxes. If a box has n bright balls in it, add one tick to the bright histogram for that number.
+        n = box_collection2[i].how_many_bright();//how many bright balls in the i_th box
+        histogram_bright[n] += 1;//add a tick to the appropriate number in the histogram
+    }
+
+    //populate histogram of number of boxes with 0,1,2, or 3 dark balls in them
+    vector<int> histogram_dark(box_collection2[0].size_of_box()+1);
+
+    for (int i=0; i <= box_collection2[0].size_of_box(); i++){ //zero out bright histogram
+        histogram_dark.push_back(0);
+    }
+
+    for (int i=0; i < num_multi_boxes; i++){ //Run through the boxes. If a box has n dark balls in it, add one tick to the dark histogram for that number.
+        n = box_collection2[i].how_many_dark();//how many dark balls in the i_th box
+        histogram_dark[n] += 1;//add a tick to the appropriate number in the histogram
+    }
+
+    //output bright ball histogram to file
+    ofstream bright_histogramfile;
+    bright_histogramfile.open("bright_histogram_file.txt");
+    bright_histogramfile << "Below are the absolute counts of 0, 1, 2, or 3 bright balls obtained by" << endl;
+    bright_histogramfile << "distributing " << num_dark_balls << " dark balls and " << num_bright_balls << " light balls among " << num_multi_boxes << " boxes." << endl ;
+    for (int i = 0; i <= box_collection2[0].size_of_box(); i++){
+        bright_histogramfile << i << ' ' << histogram_bright[i] << endl;
+    }
+    bright_histogramfile << endl << endl;
+
+    int nbalobb = 0; // Number Boxes At Least One Bright Ball
+    for (int i = 1; i <= box_collection2[0].size_of_box(); i++){
+        nbalobb += histogram_bright[i];
+    }
+
+    // Now output the normalized histogram for the bright balls.
+    bright_histogramfile << "Below are the fractions of visible boxes that are occupied with 1, 2, or 3 bright balls " << endl;
+    bright_histogramfile << "obtained by distributing " << num_dark_balls << " dark balls and " << num_bright_balls << " light balls among " << num_multi_boxes << " boxes." << endl ;
+    for (int i = 1; i <= box_collection2[0].size_of_box(); i++){
+        bright_histogramfile << i << ' ' << static_cast<double>(histogram_bright[i])/nbalobb << endl;
+    }
+
+    //output dark ball histogram to file
+    ofstream dark_histogramfile;
+    dark_histogramfile.open("dark_histogram_file.txt");
+    dark_histogramfile << "Below are the absolute counts of 0, 1, 2, or 3 dark balls obtained by" << endl;
+    dark_histogramfile << "distributing " << num_dark_balls << " dark balls and " << num_bright_balls << " light balls among " << num_multi_boxes << " boxes." << endl ;
+    for (int i = 0; i <= box_collection2[0].size_of_box(); i++){
+        dark_histogramfile << i << ' ' << histogram_dark[i] << endl;
+    }
+    dark_histogramfile << endl;
+
+    // Now output the normalized histogram for the dark balls.
+
+    delete [] box_collection2; // memory cleanup
 
     return 0;
 }
@@ -180,7 +295,7 @@ void Box::add_ball() {
 
 Multicolor_Box::Multicolor_Box(){
     Box{}; //Default constructor of base class would have been called anyways, but
-           //I am explicitly call it because it is good practice.
+           //I am explicitly calling it because it is good practice.
     _num_bright_balls = 0;
     _num_dark_balls = 0;
 }
@@ -228,3 +343,35 @@ int Multicolor_Box::how_many_bright(){
 int Multicolor_Box::how_many_dark(){
     return _num_dark_balls;
 }
+
+string Ball_collection::remove_ball(){ //randomly removes a ball from the collection. Return whether removed ball is "bright" or "dark"
+    double lb = 0;
+    double ub = 1.0;
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count(); //set the seed for RNG based on the current time
+    static uniform_real_distribution<double> unif(lb, ub);
+    static default_random_engine re(seed);
+    double r = unif(re);
+    cout << "here is a random number between 0 and 1 " << r << endl; //debugging purposes
+    cout << "the current ratio of bright to total balls is " << (static_cast<double>(_num_bright_balls)/static_cast<double>(_num_bright_balls + _num_dark_balls)) << endl; //debugging purposes
+    if (r <= (static_cast<double>(_num_bright_balls)/static_cast<double>(_num_bright_balls + _num_dark_balls))){
+        _num_bright_balls--;
+        cout << "bright" << endl; //debugging purposes
+        return "bright";
+    }else{
+        _num_dark_balls--;
+        cout << "dark" << endl; //debugging purposes
+        return "dark";
+    }
+
+}
+
+void Ball_collection::populate(int num_bright_balls, int num_dark_balls){
+    _num_bright_balls = num_bright_balls;
+    _num_dark_balls = num_dark_balls;
+}
+
+bool Ball_collection::is_collection_empty(){
+    return (_num_bright_balls + _num_dark_balls == 0);
+}
+
+
